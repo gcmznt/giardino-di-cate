29ec1cc7a609a77474ff3ba0b8893fbde6c31d46
Giacomo Zinetti <giacomo.zinetti@giko.it>
2018-06-15 10:15:52 +0200

    first commit

---
+++ getData.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+const url = "http://www.ilgiardinodeibimbi.it/index.asp";
+
+const getData = async () => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.focus('[name="userName"]');
+  await page.keyboard.type(process.env.USERNAME);
+  await page.focus('[name="userPwd"]');
+  await page.keyboard.type(process.env.PASSWORD);
+  await page.keyboard.press("Enter");
+
+  await page.waitForNavigation({ waitUntil: "load" });
+
+  const act = (await page.evaluate(() =>
+    Array.from(document.querySelectorAll('[id*="AttivitaBimbo"]')).map(d => ({
+      key: d.id,
+      date: d.parentElement.parentElement.previousElementSibling.querySelector(
+        "[align=left]"
+      ).innerText,
+      title: Array.from(d.querySelectorAll(".title")).map(e => e.innerText),
+      text: Array.from(d.querySelectorAll(".text")).map(e => e.innerText)
+    }))
+  )).map(a => ({
+    key: a.key,
+    date: a.date,
+    entries: a.title
+      .map((e, i) => ({
+        title: e
+          .replace("Ha Scaricato?", "💩")
+          .replace("Ha Dormito?", "😴")
+          .replace("Ha Fatto Merenda?", "🍦")
+          .replace("Annotazioni Varie:", "📝")
+          .replace("Attività Svolte?", "🤾‍")
+          .replace("Menu del Giorno:", "🍲"),
+        text: a.text[i]
+          .toLowerCase()
+          .replace(/\s*\n\s*/g, "\n")
+          .replace(/\s{2,}/g, " ")
+          .replace(/^si/g, "")
+          .replace(/^no/g, "❌")
+          // .replace(/\s+/g, " ")
+          .trim()
+          .split("\n")
+          .map(e =>
+            e
+              .replace(/ no$/g, " ❌")
+              .replace(/ tutto$/g, " ✅")
+              .replace(/ si$/g, " ✅")
+          )
+          .filter(val => val)
+      }))
+      .filter(e => e.text.length)
+      .filter(e => e.title !== "Ha Mangiato?")
+  }));
+  await browser.close();
+  return act;
+};
+
+getData()
+  .then(data => {
+    fs.writeFile("./static/data.json", JSON.stringify(data), function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("The file was saved!");
+    });
+  })
+  .catch();
---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div
+      v-for="entry in entries"
+      v-bind:key="entry.key"
+      v-bind:entry="entry"
+      class="card"
+    >
+      <h2>{{entry.date}}</h2>
+      <div
+        v-for="el in entry.entries"
+        v-bind:key="el.title"
+        class="entry"
+      >
+        <h3>{{el.title}}</h3>
+        <div>
+          <div v-for="t in el.text" :key="t">{{t}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import entries from "../static/data.json";
+// import Event from "../components/Event.vue";
+
+export default {
+  head: {
+    title: "Giardino di Cate"
+  },
+  components: {
+    // Event
+  },
+  data() {
+    return {
+      entries
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #9b59b6;
+}
+
+h2 {
+  margin-bottom: 30px;
+  text-align: center;
+  padding: 20px;
+  background-color: rgba(127, 127, 127, 0.1);
+}
+
+.card {
+  background-color: #fff;
+  margin: 20px;
+  border-radius: 7px;
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1);
+  opacity: 0.9;
+}
+
+.entry {
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: auto 1fr;
+  align-items: center;
+  padding: 10px 20px;
+  line-height: 1.4;
+}
+
+.entry div::first-letter {
+  text-transform: capitalize;
+}
+
+h3 {
+  font-size: 50px;
+}
+</style>
