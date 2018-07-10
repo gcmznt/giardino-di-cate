21bb57b1d1804ba20168082b52e6b4acb703e944
Giacomo Zinetti <giacomo.zinetti@giko.it>
2018-07-10 16:44:04 +0200

    update offline cache

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,8 @@
+const cacheName = "giardino-di-cate";
+
 self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open("giardino-di-cate").then(function(cache) {
+    caches.open(cacheName).then(function(cache) {
       return cache.addAll(["/", "/swing-ori.svg"]);
     })
   );
@@ -8,8 +10,16 @@ self.addEventListener("install", function(event) {
 
 self.addEventListener("fetch", function(event) {
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
-    })
+    fetch(event.request)
+      .then(function(response) {
+        caches.open(cacheName).then(function(cache) {
+          cache.put(event.request, response.clone());
+        });
+
+        return response;
+      })
+      .catch(function() {
+        return caches.match(event.request);
+      })
   );
 });
